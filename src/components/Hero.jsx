a76ac975dec8154344f0fd54902341900c3915e4
AlyaaRushdy <98500365+AlyaaRushdy@users.noreply.github.com>
2024-10-21 22:10:43 +0300

    added animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,8 +3,8 @@ function Hero() {
     <>
       <section className="hero text-bg-dark-subtle">
         <div className="container h-100">
-          <div className="row justify-content-center align-items-center px-1 h-100">
-            <div className="col-12 col-md-11 col-lg-10 col-xl-9">
+          <div className="row justify-content-center align-items-center px-1 h-100 animate__animated animate__zoomIn animate__delay-1s">
+            <div className="col-12 col-md-11 col-lg-10 col-xl-9 ">
               <h1 className="display-3 fw-bold mb-3 text-center text-white p-3 rounded">
                 {/* eslint-disable-next-line react/no-unescaped-entities */}
                 You'll Find Comfort in every{" "}
