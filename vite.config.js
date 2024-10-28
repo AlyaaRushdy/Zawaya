480f45f8812edb4a2757dadf2b221cf374de6c90
AlyaaRushdy <98500365+AlyaaRushdy@users.noreply.github.com>
2024-10-28 21:27:28 +0300

    moved images from public to src

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,33 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          // Disable hashing only for images in a specific folder
+          const projectsPath = "src/assets/images/projects/";
+          const projectsNewBase = "assets/images/projects/";
+          const projectsSliderPath = "src/assets/images/projects-slider/";
+          const projectsSliderNewBase = "assets/images/projects-slider/";
+          if (
+            assetInfo.name &&
+            assetInfo.originalFileName?.includes(projectsPath)
+          ) {
+            const fileName = assetInfo.originalFileName.split(projectsPath)[1];
+            return `${projectsNewBase}${fileName}`;
+          } else if (
+            assetInfo.name &&
+            assetInfo.originalFileName?.includes(projectsSliderPath)
+          ) {
+            const fileName =
+              assetInfo.originalFileName.split(projectsSliderPath)[1];
+            return `${projectsSliderNewBase}${fileName}`;
+          }
+
+          return "assets/[name]-[hash][extname]";
+        },
+      },
+    },
+  },
 });
