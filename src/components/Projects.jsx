b700df89c801c6260c3256b5956f4fe778890b5a
AlyaaRushdy <98500365+AlyaaRushdy@users.noreply.github.com>
2024-10-28 11:36:23 +0300

    changed the import method and added new albums and images to projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,176 +3,72 @@ import React, { useState } from "react";
 import Lightbox from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/styles.css";
 
-const entryPoint = [
-  {
-    src: "/src/assets/images/projects/1/main.jpg",
-    alt: "Album 1",
-    albumName: "album1",
-  },
-
-  {
-    src: "/src/assets/images/projects/4/main.jpg",
-    alt: "Album 4",
-    albumName: "album4",
-  },
-  {
-    src: "/src/assets/images/projects/5/main.jpg",
-    alt: "Album 5",
-    albumName: "album5",
-  },
-  {
-    src: "/src/assets/images/projects/6/main.jpg",
-    alt: "Album 6",
-    albumName: "album6",
-  },
-  {
-    src: "/src/assets/images/projects/7/main.jpg",
-    alt: "Album 7",
-    albumName: "album7",
-  },
-  {
-    src: "/src/assets/images/projects/8/main.jpg",
-    alt: "Album 8",
-    albumName: "album8",
-  },
-  {
-    src: "/src/assets/images/projects/9/main.jpg",
-    alt: "Album 9",
-    albumName: "album9",
-  },
-  {
-    src: "/src/assets/images/projects/2/main.jpg",
-    alt: "Album 2",
-    albumName: "album2",
-  },
-
-  {
-    src: "/src/assets/images/projects/10/main.jpg",
-    alt: "Album 10",
-    albumName: "album10",
-  },
-  {
-    src: "/src/assets/images/projects/11/main.jpg",
-    alt: "Album 11",
-    albumName: "album11",
-  },
-  {
-    src: "/src/assets/images/projects/3/main.jpg",
-    alt: "Album 3",
-    albumName: "album3",
-  },
-];
-
-const albums = {
-  album1: [
-    { src: "/src/assets/images/projects/1/main.jpg" },
-    { src: "/src/assets/images/projects/1/1.jpg" },
-    { src: "/src/assets/images/projects/1/2.jpg" },
-    { src: "/src/assets/images/projects/1/3.jpg" },
-  ],
-  album2: [
-    { src: "/src/assets/images/projects/2/main.jpg" },
-    { src: "/src/assets/images/projects/2/1.jpg" },
-    { src: "/src/assets/images/projects/2/2.jpg" },
-    { src: "/src/assets/images/projects/2/3.jpg" },
-    { src: "/src/assets/images/projects/2/4.jpg" },
-  ],
-  album3: [
-    { src: "/src/assets/images/projects/3/main.jpg" },
-    { src: "/src/assets/images/projects/3/1.jpg" },
-    { src: "/src/assets/images/projects/3/2.jpg" },
-    { src: "/src/assets/images/projects/3/3.jpg" },
-    { src: "/src/assets/images/projects/3/4.jpg" },
-    { src: "/src/assets/images/projects/3/5.jpg" },
-  ],
-  album4: [
-    { src: "/src/assets/images/projects/4/main.jpg" },
-    { src: "/src/assets/images/projects/4/1.jpg" },
-    { src: "/src/assets/images/projects/4/2.jpg" },
-    { src: "/src/assets/images/projects/4/3.jpg" },
-    { src: "/src/assets/images/projects/4/4.jpg" },
-    { src: "/src/assets/images/projects/4/5.jpg" },
-    { src: "/src/assets/images/projects/4/6.jpg" },
-    { src: "/src/assets/images/projects/4/7.jpg" },
-    { src: "/src/assets/images/projects/4/8.jpg" },
-    { src: "/src/assets/images/projects/4/9.jpg" },
-    { src: "/src/assets/images/projects/4/10.jpg" },
-    { src: "/src/assets/images/projects/4/11.jpg" },
-    { src: "/src/assets/images/projects/4/12.jpg" },
-  ],
-  album5: [
-    { src: "/src/assets/images/projects/5/main.jpg" },
-    { src: "/src/assets/images/projects/5/1.jpg" },
-    { src: "/src/assets/images/projects/5/2.jpg" },
-    { src: "/src/assets/images/projects/5/3.jpg" },
-    { src: "/src/assets/images/projects/5/4.jpg" },
-    { src: "/src/assets/images/projects/5/5.jpg" },
-  ],
-  album6: [
-    { src: "/src/assets/images/projects/6/main.jpg" },
-    { src: "/src/assets/images/projects/6/1.jpg" },
-    { src: "/src/assets/images/projects/6/2.jpg" },
-    { src: "/src/assets/images/projects/6/3.jpg" },
-    { src: "/src/assets/images/projects/6/4.jpg" },
-    { src: "/src/assets/images/projects/6/5.jpg" },
-  ],
-  album7: [
-    { src: "/src/assets/images/projects/7/main.jpg" },
-    { src: "/src/assets/images/projects/7/1.jpg" },
-    { src: "/src/assets/images/projects/7/2.jpg" },
-    { src: "/src/assets/images/projects/7/3.jpg" },
-    { src: "/src/assets/images/projects/7/4.jpg" },
-    { src: "/src/assets/images/projects/7/5.jpg" },
-    { src: "/src/assets/images/projects/7/6.jpg" },
-  ],
-  album8: [
-    { src: "/src/assets/images/projects/8/main.jpg" },
-    { src: "/src/assets/images/projects/8/1.jpg" },
-    { src: "/src/assets/images/projects/8/2.jpg" },
-    { src: "/src/assets/images/projects/8/3.jpg" },
-    { src: "/src/assets/images/projects/8/4.jpg" },
-    { src: "/src/assets/images/projects/8/5.jpg" },
-    { src: "/src/assets/images/projects/8/6.jpg" },
-    { src: "/src/assets/images/projects/8/7.jpg" },
-    { src: "/src/assets/images/projects/8/8.jpg" },
-    { src: "/src/assets/images/projects/8/9.jpg" },
-  ],
-  album9: [
-    { src: "/src/assets/images/projects/9/main.jpg" },
-    { src: "/src/assets/images/projects/9/1.jpg" },
-    { src: "/src/assets/images/projects/9/2.jpg" },
-    { src: "/src/assets/images/projects/9/3.jpg" },
-    { src: "/src/assets/images/projects/9/4.jpg" },
-    { src: "/src/assets/images/projects/9/5.jpg" },
-    { src: "/src/assets/images/projects/9/6.jpg" },
-    { src: "/src/assets/images/projects/9/7.jpg" },
-    { src: "/src/assets/images/projects/9/8.jpg" },
-    { src: "/src/assets/images/projects/9/9.jpg" },
-  ],
-  album10: [
-    { src: "/src/assets/images/projects/10/main.jpg" },
-    { src: "/src/assets/images/projects/10/1.jpg" },
-    { src: "/src/assets/images/projects/10/2.jpg" },
-    { src: "/src/assets/images/projects/10/3.jpg" },
-    { src: "/src/assets/images/projects/10/4.jpg" },
-    { src: "/src/assets/images/projects/10/5.jpg" },
-  ],
-  album11: [
-    { src: "/src/assets/images/projects/11/main.jpg" },
-    { src: "/src/assets/images/projects/11/1.jpg" },
-    { src: "/src/assets/images/projects/11/2.jpg" },
-    { src: "/src/assets/images/projects/11/3.jpg" },
-    { src: "/src/assets/images/projects/11/4.jpg" },
-    { src: "/src/assets/images/projects/11/5.jpg" },
-    { src: "/src/assets/images/projects/11/6.jpg" },
-    { src: "/src/assets/images/projects/11/7.jpg" },
-    { src: "/src/assets/images/projects/11/8.jpg" },
-    { src: "/src/assets/images/projects/11/9.jpg" },
-    { src: "/src/assets/images/projects/11/10.jpg" },
-    { src: "/src/assets/images/projects/11/11.jpg" },
-  ],
+const importAllAlbumsObject = {
+  album1: import.meta.glob(
+    "/src/assets/images/projects/1/*.{jpg,jpeg,png,gif}"
+  ),
+  album2: import.meta.glob(
+    "/src/assets/images/projects/2/*.{jpg,jpeg,png,gif}"
+  ),
+  album3: import.meta.glob(
+    "/src/assets/images/projects/3/*.{jpg,jpeg,png,gif}"
+  ),
+  album4: import.meta.glob(
+    "/src/assets/images/projects/4/*.{jpg,jpeg,png,gif}"
+  ),
+  album5: import.meta.glob(
+    "/src/assets/images/projects/5/*.{jpg,jpeg,png,gif}"
+  ),
+  album6: import.meta.glob(
+    "/src/assets/images/projects/6/*.{jpg,jpeg,png,gif}"
+  ),
+  album7: import.meta.glob(
+    "/src/assets/images/projects/7/*.{jpg,jpeg,png,gif}"
+  ),
+  album8: import.meta.glob(
+    "/src/assets/images/projects/8/*.{jpg,jpeg,png,gif}"
+  ),
+  album9: import.meta.glob(
+    "/src/assets/images/projects/9/*.{jpg,jpeg,png,gif}"
+  ),
+  album10: import.meta.glob(
+    "/src/assets/images/projects/10/*.{jpg,jpeg,png,gif}"
+  ),
+  album11: import.meta.glob(
+    "/src/assets/images/projects/11/*.{jpg,jpeg,png,gif}"
+  ),
+  album12: import.meta.glob(
+    "/src/assets/images/projects/12/*.{jpg,jpeg,png,gif}"
+  ),
+  album13: import.meta.glob(
+    "/src/assets/images/projects/13/*.{jpg,jpeg,png,gif}"
+  ),
+  album14: import.meta.glob(
+    "/src/assets/images/projects/14/*.{jpg,jpeg,png,gif}"
+  ),
+  album15: import.meta.glob(
+    "/src/assets/images/projects/15/*.{jpg,jpeg,png,gif}"
+  ),
 };
 
+const importedAlbumsValues = Object.values(importAllAlbumsObject);
+const albums = {};
+for (let i = 0; i < importedAlbumsValues.length; i++) {
+  const album = Object.values(importedAlbumsValues[i]).reduce((prev, curr) => {
+    prev.push({ src: curr.name });
+    return prev;
+  }, []);
+  albums[`album${i + 1}`] = album;
+}
+const entryPoints = [];
+Object.values(albums).forEach((album, index) => {
+  const imagesArray = Object.values(album);
+  entryPoints.push({
+    src: imagesArray[0].src,
+    alt: `album ${index + 1}`,
+    albumName: `album${index + 1}`,
+  });
+});
 function Projects() {
   const [open, setOpen] = useState(false);
   const [currentAlbum, setCurrentAlbum] = useState([]);
@@ -193,12 +89,16 @@ function Projects() {
             columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}
           >
             <Masonry columnsCount={3} gutter="10px">
-              {entryPoint.map((image, i) => (
+              {entryPoints.map((image, i) => (
                 <img
                   key={i}
                   src={image.src}
                   alt={image.alt}
-                  style={{ width: "100%", display: "block", cursor: "pointer" }}
+                  style={{
+                    width: "100%",
+                    display: "block",
+                    cursor: "pointer",
+                  }}
                   onClick={() => openAlbum(image.albumName)}
                 />
               ))}
