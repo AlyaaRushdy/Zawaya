cf923b3fe87bd506b416ebb2fa72a1b23da8f24e
AlyaaRushdy <98500365+AlyaaRushdy@users.noreply.github.com>
2024-10-29 01:25:28 +0300

    imported clients images

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,6 +1,14 @@
 import Slider from "react-slick";
+import client1 from "../assets/images/clients/clients1.png";
+import client2 from "../assets/images/clients/clients2.png";
+import client3 from "../assets/images/clients/clients3.png";
+import client4 from "../assets/images/clients/clients4.png";
+import client5 from "../assets/images/clients/clients5.png";
+import client6 from "../assets/images/clients/clients6.webp";
 
 function Clients() {
+  const images = [client1, client2, client3, client4, client5, client6];
+
   var settings = {
     dots: false,
     infinite: true,
@@ -33,62 +41,18 @@ function Clients() {
     <>
       <section className="bg-body-tertiary">
         <div className="container clients">
+          {console.log(images)}
           <Slider {...settings}>
-            <div className="p-3 mx-auto">
-              <img
-                src="/src/assets/images/clients/clients1.png"
-                className="img-fluid mx-auto"
-                alt=""
-              />
-            </div>
-
-            <div className="p-3 mx-auto">
-              <img
-                src="/src/assets/images/clients/clients2.png"
-                className="img-fluid mx-auto"
-                alt=""
-              />
-            </div>
-
-            {/* <div className="p-3 mx-auto">
-              <img
-                src="/src/assets/images/clients/clients3.jpg"
-                className="img-fluid mx-auto"
-                alt=""
-              />
-            </div> */}
-
-            <div className="p-3 mx-auto">
-              <img
-                src="/src/assets/images/clients/clients3.png"
-                className="img-fluid mx-auto"
-                alt=""
-              />
-            </div>
-
-            <div className="p-3 mx-auto">
-              <img
-                src="/src/assets/images/clients/clients4.png"
-                className="img-fluid mx-auto"
-                alt=""
-              />
-            </div>
-
-            <div className="p-3 mx-auto">
-              <img
-                src="/src/assets/images/clients/clients5.png"
-                className="img-fluid mx-auto"
-                alt=""
-              />
-            </div>
-
-            <div className="p-3 mx-auto">
-              <img
-                src="/src/assets/images/clients/clients6.webp"
-                className="img-fluid mx-auto"
-                alt=""
-              />
-            </div>
+            {images.map((image, i) => (
+              <div className="p-3 ">
+                <img
+                  src={image}
+                  className="img-fluid mx-auto"
+                  key={i}
+                  alt={`client ${i + 1} image`}
+                />
+              </div>
+            ))}
           </Slider>
         </div>
       </section>
