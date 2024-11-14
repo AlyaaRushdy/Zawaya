f413dfb36a094121ef38de579e4dc36d772c3b4b
AlyaaRushdy <98500365+AlyaaRushdy@users.noreply.github.com>
2024-11-14 20:17:59 +0200

    hero section became a carousel

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,69 @@
+import hero1 from "../assets/images/hero/hero1.jpg";
+import hero2 from "../assets/images/hero/hero2.jpg";
+import hero3 from "../assets/images/hero/hero3.jpg";
+
 function Hero() {
   return (
     <>
-      <section className="hero text-bg-dark-subtle" id="hero">
-        <div className="container h-100">
-          <div className="row justify-content-center align-items-center px-1 h-100 animate__animated animate__zoomIn animate__delay-1s">
-            <div className="col-12 col-md-11 col-lg-10 col-xl-9 ">
-              <h1 className="display-3 fw-bold mb-3 text-center text-white p-3 rounded">
-                {/* eslint-disable-next-line react/no-unescaped-entities */}
-                You'll Find Comfort in every{" "}
-                <span className="text-primary fw-bolder">ZAWYA</span> of your
-                place
-              </h1>
+      <section
+        id="hero-carousel"
+        class="carousel slide"
+        data-bs-ride="carousel"
+        data-bs-pause="false"
+      >
+        <div class="carousel-inner">
+          <div class="carousel-item h-100 active">
+            <img
+              src={hero1}
+              class="d-block w-100 h-100 object-fit-cover"
+              alt="hero-image"
+            />
+            <div class="carousel-caption text-start">
+              {/*d-none d-md-block*/}
+              <h4>3D Modeling</h4>
+            </div>
+          </div>
+          <div class="carousel-item h-100">
+            <img
+              src={hero2}
+              class="d-block w-100 h-100 object-fit-cover"
+              alt="hero-image"
+            />
+            <div class="carousel-caption text-start">
+              {/*d-none d-md-block*/}
+              <h4>Exterior</h4>
+            </div>
+          </div>
+          <div class="carousel-item h-100">
+            <img
+              src={hero3}
+              class="d-block w-100 h-100 object-fit-cover"
+              alt="hero-image"
+            />
+            <div class="carousel-caption text-start">
+              {/*d-none d-md-block*/}
+              <h4>Office Interior Design</h4>
             </div>
           </div>
         </div>
+        <button
+          class="carousel-control-prev"
+          type="button"
+          data-bs-target="#hero-carousel"
+          data-bs-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-bs-target="#hero-carousel"
+          data-bs-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
       </section>
     </>
   );
