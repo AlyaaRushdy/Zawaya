19072be8c2632acbace4232874126e0a13fb41e2
AlyaaRushdy <alyaaahmed2502@gmail.com>
2026-08-02 01:27:36 +0300

    modified projects sequence

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,51 +7,49 @@ const basePath = import.meta.env.DEV ? "/src/" : "/";
 
 const importAllAlbumsObject = {
   album1: import.meta.glob(
-    "/src/assets/images/projects/01/*.{jpg,jpeg,png,gif}"
+    "/src/assets/images/projects/01/*.{jpg,jpeg,png,gif}",
   ),
   album2: import.meta.glob(
-    "/src/assets/images/projects/02/*.{jpg,jpeg,png,gif}"
+    "/src/assets/images/projects/02/*.{jpg,jpeg,png,gif}",
   ),
   album3: import.meta.glob(
-    "/src/assets/images/projects/03/*.{jpg,jpeg,png,gif}"
+    "/src/assets/images/projects/03/*.{jpg,jpeg,png,gif}",
   ),
   album4: import.meta.glob(
-    "/src/assets/images/projects/04/*.{jpg,jpeg,png,gif}"
+    "/src/assets/images/projects/04/*.{jpg,jpeg,png,gif}",
   ),
   album5: import.meta.glob(
-    "/src/assets/images/projects/05/*.{jpg,jpeg,png,gif}"
+    "/src/assets/images/projects/05/*.{jpg,jpeg,png,gif}",
   ),
   album6: import.meta.glob(
-    "/src/assets/images/projects/06/*.{jpg,jpeg,png,gif}"
+    "/src/assets/images/projects/06/*.{jpg,jpeg,png,gif}",
   ),
   album7: import.meta.glob(
-    "/src/assets/images/projects/07/*.{jpg,jpeg,png,gif}"
+    "/src/assets/images/projects/07/*.{jpg,jpeg,png,gif}",
   ),
   album8: import.meta.glob(
-    "/src/assets/images/projects/08/*.{jpg,jpeg,png,gif}"
+    "/src/assets/images/projects/08/*.{jpg,jpeg,png,gif}",
   ),
   album9: import.meta.glob(
-    "/src/assets/images/projects/09/*.{jpg,jpeg,png,gif}"
+    "/src/assets/images/projects/09/*.{jpg,jpeg,png,gif}",
   ),
   album10: import.meta.glob(
-    "/src/assets/images/projects/10/*.{jpg,jpeg,png,gif}"
+    "/src/assets/images/projects/10/*.{jpg,jpeg,png,gif}",
   ),
   album11: import.meta.glob(
-    "/src/assets/images/projects/11/*.{jpg,jpeg,png,gif}"
+    "/src/assets/images/projects/11/*.{jpg,jpeg,png,gif}",
   ),
   album12: import.meta.glob(
-    "/src/assets/images/projects/12/*.{jpg,jpeg,png,gif}"
+    "/src/assets/images/projects/12/*.{jpg,jpeg,png,gif}",
   ),
   album13: import.meta.glob(
-    "/src/assets/images/projects/13/*.{jpg,jpeg,png,gif}"
+    "/src/assets/images/projects/13/*.{jpg,jpeg,png,gif}",
   ),
   album14: import.meta.glob(
-    "/src/assets/images/projects/14/*.{jpg,jpeg,png,gif}"
+    "/src/assets/images/projects/14/*.{jpg,jpeg,png,gif}",
   ),
-  // album15: import.meta.glob(
-  //   "/src/assets/images/projects/15/*.{jpg,jpeg,png,gif}"
-  // ),
 };
+
 const importedAlbumsValues = Object.values(importAllAlbumsObject);
 const albums = {};
 for (let i = 0; i < importedAlbumsValues.length; i++) {
@@ -61,6 +59,7 @@ for (let i = 0; i < importedAlbumsValues.length; i++) {
   }, []);
   albums[`album${i + 1}`] = album;
 }
+
 const entryPoints = [];
 Object.values(albums).forEach((album, index) => {
   const imagesArray = Object.values(album);
@@ -70,6 +69,7 @@ Object.values(albums).forEach((album, index) => {
     albumName: `album${index + 1}`,
   });
 });
+
 function Projects() {
   const [open, setOpen] = useState(false);
   const [currentAlbum, setCurrentAlbum] = useState([]);
@@ -81,6 +81,10 @@ function Projects() {
     setStartIndex(0);
   };
 
+  const handleShowMore = () => {
+    setVisibleCount(14);
+  };
+
   return (
     <>
       <section className="pt-4 pb-5" id="projects">
@@ -89,7 +93,6 @@ function Projects() {
           <ResponsiveMasonry
             columnsCountBreakPoints={{ 350: 1, 750: 2, 991: 3 }}
           >
-            {/* animate__fadeInUp  animate__backInUp animate__fadeInUpBig  */}
             <Masonry columnsCount={3} gutter="10px">
               {entryPoints.map((image, i) => (
                 <ScrollAnimation
